bb74624795ee1035bf127fae2736febf462532cb
Jacky <jacky.ng9188@gmail.com>
2019-07-01 13:42:20 -0400

    Added a Load More button to display more questions retrieved from API through Django's pagination feature.

--- Querios/frontend/src/views/Home.vue
+++ Querios/frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="container">
+    <div class="container mt-2">
       <div v-for="question in questions"
            :key="question.pk">
         <p class="mb-0">Posted by:
@@ -17,6 +17,14 @@
         <p>Answers: {{ question.answers_count }}</p>
         <hr>
       </div>
+      <div class="my-4">
+        <p v-show="loadingQuestions">...loading...</p>
+        <button v-show="next"
+                @click="getQuestions"
+                class="btn btn-sm btn-outline-success">
+                Load More
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -28,15 +36,37 @@ export default {
   name: "home",
   data : function () {
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions: false
     }
   },
   methods: {
     getQuestions() {
+      /* 
+      makes a GET request to the questions list API endpoint. 
+      When this getQuestions methos is called for the first time by the Vue created()
+      lifecycle hook, we check if there's another page with the if (data.next) block.
+      If it exists, it says this.next to that endpoint's next. We can then call a 
+      loadMore button to load more questions which calls if (this.next){ endpoint = this.next;}
+      */
       let endpoint = "api/questions/";
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingQuestions = true;
       apiService(endpoint)
         .then(data => {
           this.questions.push(...data.results)
+          // Our REST API response from Django has a next field that we can use to assign
+          // to the next field. We also need to set loadingQuestion back to false after the push.
+          this.loadingQuestions = false;
+          if (data.next) {
+            this.next = data.next;
+          }
+          else {
+            this.next = null;
+          }
         })
     }
   },
