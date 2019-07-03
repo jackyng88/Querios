64d21e6a388591d6606e8a258efadf07cd4c6f2a
Jacky <jacky.ng9188@gmail.com>
2019-07-03 16:18:38 -0400

    Added Load More answers feature, currently changed the Django REST pagination size to two objects per page for testing.

--- Querios/Querios/settings.py
+++ Querios/Querios/settings.py
@@ -187,7 +187,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4
+    'PAGE_SIZE': 2
 }
 
 # Config for webpack to allow for proper injection 
